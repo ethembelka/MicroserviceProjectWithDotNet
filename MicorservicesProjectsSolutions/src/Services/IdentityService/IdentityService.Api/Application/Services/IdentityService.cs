7849be56018f16862ec5d39a0ee43a1ae2e9770a
ethembelka <ethembelka@gmail.com>
2023-11-18 17:23:05 +0300

    IdentityService created.

---
+++ MicorservicesProjectsSolutions/src/Services/IdentityService/IdentityService.Api/Application/Services/IdentityService.cs
@@ -0,0 +1,38 @@
+﻿using IdentityService.Api.Application.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityService.Api.Application.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<LoginResponseModel> Login(LoginRequestModel requestModel)
+        {
+            // DB Process will be here. Check user information is valid and get details.
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, requestModel.UserName),
+                new Claim(ClaimTypes.Name, "Ethem Sahin")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MicroservicesProjectSecretKeyShouldBeLong"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.Now.AddDays(10);
+
+            var token = new JwtSecurityToken(claims: claims, expires: expiry, notBefore: DateTime.Now, signingCredentials: creds);
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            LoginResponseModel response = new()
+            {
+                UserToken = encodedJwt,
+                UserName = requestModel.UserName
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
